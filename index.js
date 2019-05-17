1fc9570fd2298058e91ed66a0d2edb96852d616b
James Wigdor <jpwigdor@gmail.com>
2019-05-16 23:30:22 -0400

    changed project that was deployed. mirrored WebAPI-II

---
+++ index.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+// console.log("process env", process.env);
+
+const server = require("./api/server.js");
+const PORT = process.env.PORT || 4000;
+
+server.listen(PORT, () => {
+  console.log(`\n*** Server Running on port ${PORT}***\n`);
+});
